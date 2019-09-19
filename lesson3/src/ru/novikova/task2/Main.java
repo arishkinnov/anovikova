70dfdc9cbc288e58e679dcb18d47bb3498a878d2
a.novikova <a.novikova.stc@innopolis.university>
2019-09-19 21:17:58 +0300

    Добавлен урок 3

---
+++ lesson3/src/ru/novikova/task2/Main.java
@@ -0,0 +1,17 @@
+package ru.novikova.task2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int gross = Integer.parseInt(reader.readLine());
+        double net = gross * 0.87;
+        System.out.println( (int) net + " руб.");
+    }
+
+}
+
