2d15bdd81cf33885f73a908d407a9e7eea3986b7
a.novikova <a.novikova.stc@innopolis.university>
2019-09-28 22:38:59 +0300

    Добавлено задание 11

---
+++ src/ru/anovikova/lesson11/Main.java
@@ -0,0 +1,25 @@
+package ru.anovikova.lesson11;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = reader.readLine();
+
+        String regex = "бяка";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(line);
+        String censorship = matcher.replaceAll("вырезано цензурой");
+        System.out.println(censorship);
+
+
+
+    }
+}
