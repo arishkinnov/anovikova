2b815141d59151eadb649a97fbc326fa52e9a577
a.novikova <a.novikova.stc@innopolis.university>
2019-10-07 13:44:19 +0300

    lesson 13 добавлено вывод на экран на латинице

--- src/ru/anovikova/lesson13/Child.java
+++ src/ru/anovikova/lesson13/Child.java
@@ -8,9 +8,9 @@ public class Child {
                 case JUICE:
                 case APPLE:
                 case CARROT:
-                    return "Съел за обе щеки " + food;
+                    return "Съел за обе щеки " + food.getTitle();
                 default:
-                    throw new ChildException("Я не люблю " + food);
+                    throw new ChildException("Я не люблю " + food.getTitle());
             }
         } finally {
             System.out.println("Спасибо, Мама!");
--- src/ru/anovikova/lesson13/Food.java
+++ src/ru/anovikova/lesson13/Food.java
@@ -1,5 +1,19 @@
 package ru.anovikova.lesson13;
 
 public enum Food {
-    CARROT, APPLE, JUICE, PORRIDGE, SOUP
+    CARROT("каша"),
+    APPLE("яблоко"),
+    JUICE("сок"),
+    PORRIDGE("каша"),
+    SOUP("суп");
+
+    private final String title;
+
+    Food(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
