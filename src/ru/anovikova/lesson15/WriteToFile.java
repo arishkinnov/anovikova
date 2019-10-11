c8e745a55be2206e1286d9d3c45c4661ec8f0dec
a.novikova <a.novikova.stc@innopolis.university>
2019-10-11 21:50:43 +0300

    lesson 15 Добавлено копирование файла

--- src/ru/anovikova/lesson15/WriteToFile.java
+++ src/ru/anovikova/lesson15/WriteToFile.java
@@ -1,10 +1,13 @@
 package ru.anovikova.lesson15;
 
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WriteToFile {
 
-    public static  void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         File file = new File("newFile.txt");
         try {
             file.createNewFile();      // создаем файл newFile.txt
@@ -17,9 +20,18 @@ public class WriteToFile {
         fw.write("Привет,Мир!");
         fw.close();
 
-        File destFile = new File("dex.txt");
+
+        File destFile = new File("dex.txt");   //переименовываем файл newFile.txt в dex.txt
         boolean renamed = file.renameTo(destFile);
-        System.out.println("Renamed: "+ renamed);
+        System.out.println("Renamed: " + renamed);
+
+
+        File source = new File("source.txt");   // создаем новый файл source
+        Files.copy(file.toPath(), source.toPath());       // копируем содержание файла file в файл source
+
+
+
+
 
 
 
