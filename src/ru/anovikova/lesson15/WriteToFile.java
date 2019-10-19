e7cb981bae2135426c90a1303c79e483a5e832e0
a.novikova <a.novikova.stc@innopolis.university>
2019-10-19 11:27:52 +0300

    lesson 15 метод main поделен на мелкие методы

--- src/ru/anovikova/lesson15/WriteToFile.java
+++ src/ru/anovikova/lesson15/WriteToFile.java
@@ -3,19 +3,64 @@ package ru.anovikova.lesson15;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class WriteToFile {
 
     public static void main(String[] args) throws IOException {
-        File filesDirectory = new File("files");
-        System.out.println("Создаем директорию files");
-        if (!filesDirectory.exists()) {
-            filesDirectory.mkdir();
-        }
+        File filesDirectory = directoryCreateFile();
         readFolder(new File("src"), 0);
         readFolder(filesDirectory, 0);
 
+        File newFile = newFile();
+
+
+        writeToFile(filesDirectory, newFile);
+
+        File destFile = renameToFile(filesDirectory, newFile);
+
+        File source = copyToFile(filesDirectory, destFile);
+
+        deleteToFile(filesDirectory, destFile, source);
+
+    }
+
+    private static void deleteToFile(File filesDirectory, File destFile, File source) throws IOException {
+        System.out.println("Удаляем файл files/dex.txt");
+        Files.delete(destFile.toPath());
+        readFolder(filesDirectory, 0);
+        System.out.println("Удаляем файл files/source.txt");
+        Files.delete(source.toPath());
+        readFolder(filesDirectory, 0);
+    }
+
+    private static File copyToFile(File filesDirectory, File destFile) throws IOException {
+        System.out.println("Копируем файл files/dex.txt в files/source.txt");
+        File source = new File("files/source.txt");   // создаем новый файл source
+        Files.copy(destFile.toPath(), source.toPath());       // копируем содержание файла file в файл source
+
+        readFolder(filesDirectory, 0);
+        return source;
+    }
+
+    private static File renameToFile(File filesDirectory, File newFile) {
+        System.out.println("Переименовываем файл files/newFile.txt в files/dex.txt");
+        File destFile = new File("files/dex.txt");   //переименовываем файл newFile.txt в dex.txt
+        boolean renamed = newFile.renameTo(destFile);
+        System.out.println("Renamed: " + renamed);
+
+        readFolder(filesDirectory, 0);
+        return destFile;
+    }
+
+    private static void writeToFile(File filesDirectory, File newFile) throws IOException {
+        FileWriter fw = new FileWriter(newFile); // записываем в файл Привет Мир!
+        fw.write("Привет,Мир!");
+        fw.close();
+
+        readFolder(filesDirectory, 0);
+    }
+
+    private static File newFile() {
         System.out.println("Создаем файл files/newFile.txt");
         File newFile = new File("files/newFile.txt");
         try {
@@ -24,34 +69,16 @@ public class WriteToFile {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return newFile;
+    }
 
-
-        FileWriter fw = new FileWriter(newFile); // записываем в файл Привет Мир!
-        fw.write("Привет,Мир!");
-        fw.close();
-
-        readFolder(filesDirectory, 0);
-
-        System.out.println("Переименовываем файл files/newFile.txt в files/dex.txt");
-        File destFile = new File("files/dex.txt");   //переименовываем файл newFile.txt в dex.txt
-        boolean renamed = newFile.renameTo(destFile);
-        System.out.println("Renamed: " + renamed);
-
-        readFolder(filesDirectory, 0);
-
-        System.out.println("Копируем файл files/dex.txt в files/source.txt");
-        File source = new File("files/source.txt");   // создаем новый файл source
-        Files.copy(destFile.toPath(), source.toPath());       // копируем содержание файла file в файл source
-
-        readFolder(filesDirectory, 0);
-
-        System.out.println("Удаляем файл files/dex.txt");
-        Files.delete(destFile.toPath());
-        readFolder(filesDirectory, 0);
-        System.out.println("Удаляем файл files/source.txt");
-        Files.delete(source.toPath());
-        readFolder(filesDirectory, 0);
-
+    private static File directoryCreateFile() {
+        File filesDirectory = new File("files");
+        System.out.println("Создаем директорию files");
+        if (!filesDirectory.exists()) {
+            filesDirectory.mkdir();
+        }
+        return filesDirectory;
     }
 
     private static void readFolder(File directory, int level) {
