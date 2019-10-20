430d3f857ca84ad7f7260a5f5dd4f8f9be871ad3
arina <aribal@mail.ru>
2019-10-20 09:47:23 +0300

    lesson19 main  разбит на мелкие методы

--- src/ru/anovikova/lesson19/CashVoucher.java
+++ src/ru/anovikova/lesson19/CashVoucher.java
@@ -2,7 +2,6 @@ package ru.anovikova.lesson19;
 
 import java.io.*;
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,49 +15,53 @@ public class CashVoucher {
     public static void main(String[] args) {
             try(Scanner scanner = new Scanner(new File("files/lesson19/products.txt"))) {
                 scanner.useDelimiter("\n");
-                ArrayList<Product> products = new ArrayList<>();
-                int count = 0;
-                Product product = null;
-                while (scanner.hasNext()) {
-                    FieldType fieldType;
-                    if (count < 3) {
-                        fieldType = FieldType.findByIndex(count);
-                    } else {
-                        fieldType = FieldType.findByIndex(count % 3);
-                    }
-                    boolean isNewProduct = FieldType.TITLE == fieldType;
-                    if (isNewProduct) {
-                        product = new Product();
-                    }
-
-                    String value = scanner.next();
-                    fillProduct(product, fieldType, value);
-
-                    boolean isEndProduct = FieldType.AMOUNT == fieldType;
-                    if (isEndProduct) {
-                        products.add(product);
-                    }
-
-                    count++;
-                }
-                System.out.printf(HEADER_FORMAT, "Наименование", "Цена", "Кол-во", "Стоимость");
-                System.out.println("==================================================");
-                BigDecimal total = new BigDecimal(0).setScale(2, ROUND_HALF_EVEN);
-                for (Product p : products) {
-                    String sumString = String.format("%.2f", p.getAmount() * p.getPrice());
-                    total = total.add(new BigDecimal(p.getAmount() * p.getPrice()).setScale(2, ROUND_HALF_EVEN));
-                    System.out.printf(ROW_FORMAT, p.getTitle(), p.getPrice(), p.getAmount(), String.format("=%s", sumString));
-                }
-                System.out.println("==================================================");
-                System.out.printf(TOTAL_FORMAT, "Итого:", total.doubleValue());
+                ArrayList<Product> products = readProducts(scanner);
+                printVoucher(products);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
+    }
 
+    private static ArrayList<Product> readProducts(Scanner scanner) {
+        ArrayList<Product> products = new ArrayList<>();
+        int count = 0;
+        Product product = null;
+        while (scanner.hasNext()) {
+            FieldType fieldType;
+            if (count < 3) {
+                fieldType = FieldType.findByIndex(count);
+            } else {
+                fieldType = FieldType.findByIndex(count % 3);
+            }
+            boolean isNewProduct = FieldType.TITLE == fieldType;
+            if (isNewProduct) {
+                product = new Product();
+            }
 
+            String value = scanner.next();
+            fillProduct(product, fieldType, value);
 
+            boolean isEndProduct = FieldType.AMOUNT == fieldType;
+            if (isEndProduct) {
+                products.add(product);
+            }
 
+            count++;
+        }
+        return products;
+    }
 
+    private static void printVoucher(ArrayList<Product> products) {
+        System.out.printf(HEADER_FORMAT, "Наименование", "Цена", "Кол-во", "Стоимость");
+        System.out.println("==================================================");
+        BigDecimal total = new BigDecimal(0).setScale(2, ROUND_HALF_EVEN);
+        for (Product p : products) {
+            String sumString = String.format("%.2f", p.getAmount() * p.getPrice());
+            total = total.add(new BigDecimal(p.getAmount() * p.getPrice()).setScale(2, ROUND_HALF_EVEN));
+            System.out.printf(ROW_FORMAT, p.getTitle(), p.getPrice(), p.getAmount(), String.format("=%s", sumString));
+        }
+        System.out.println("==================================================");
+        System.out.printf(TOTAL_FORMAT, "Итого:", total.doubleValue());
     }
 
     private static void fillProduct(Product product, FieldType fieldType, String value) {
